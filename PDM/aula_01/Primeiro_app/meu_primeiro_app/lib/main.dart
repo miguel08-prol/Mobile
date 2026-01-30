1dc0d0d5496e0d42b1ff14240846582273523f12
Miguel <miguelmassane05@gmail.com>
2026-01-30 16:59:02 -0300

    aula 1

---
+++ PDM/aula_01/Primeiro_app/meu_primeiro_app/lib/main.dart
@@ -0,0 +1,30 @@
+// Importa o pacote Flutter, que contém os widgets e ferramentas necessárias para criar o aplicativo.
+import 'package:flutter/material.dart';
+
+// A função principal do programa, que é o ponto de entrada da aplicação.
+void main() {
+  runApp(MyApp()); // Executa o aplicativo, iniciando o widget raiz "MyApp".
+}
+
+// Define o widget principal da aplicação.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Construtor do widget MyApp.
+  @override
+  Widget build(BuildContext context) {
+    // Retorna o MaterialApp, que é a base para aplicativos com estilo Material Design.
+    return MaterialApp(
+      home: Scaffold( // Scaffold fornece uma estrutura básica para uma tela, com barra de app, corpo e outros elementos.
+        appBar: AppBar( // Define a barra superior (AppBar) com o título do aplicativo.
+          title: Text('Hello World App'), // Título exibido na AppBar.
+        ),
+        body: Center( // Centraliza os widgets filhos na tela.
+          child: Text(
+            'Olá, Samuel!', // Exibe o texto "Olá, Samuel!" no centro da tela.
+          ),
+        ),
+      ),
+    );
+  }
+}
